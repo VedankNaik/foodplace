8e16f37ecd4c044c2fd90a87b380f79d1b78f980
Vedank Naik <vedankn01@gmail.com>
2022-10-19 16:32:09 +0530

    Chef component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import { Navbar } from "./components";
+import { Header, AboutUs, SpecialMenu, Chef } from "./container";
 
 function App() {
   return (
     <div>
       <Navbar />
+      <Header />
+      <AboutUs />
+      <SpecialMenu />
+      <Chef />
     </div>
   );
 }
